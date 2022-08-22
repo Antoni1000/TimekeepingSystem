7256663df636a5e7fb0f16eee5ca3410939b227a
Antoni1000 <90736291+Antoni1000@users.noreply.github.com>
2022-08-23 04:16:18 +0700

    Add files via upload

--- src/java/dal/EmployeeDBContext.java
+++ src/java/dal/EmployeeDBContext.java
@@ -4,10 +4,52 @@
  */
 package dal;
 
+import helper.DateTimeHelper;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Employee;
+
 /**
  *
  * @author hp
  */
-public class EmployeeDBContext {
+public class EmployeeDBContext extends DBContext{
     
+    public ArrayList<Employee> getEmps(Date from,Date to){
+        
+        ArrayList<Employee> emps = new ArrayList<>();
+        try {
+            String sql = "select e.eid, e.ename,r.rname,w.wid,t.tid,w.[from],w.[to],t.checkin,t.checkout "
+                    + "from Employee e,WorkDate w,Timesheet t,WorkingTime wt , Role r \n"
+                    + "where e.eid = wt.eid and w.wid = wt.wid and t.tid = wt.tid and r.rid = e.rid Order by e.eid";
+            PreparedStatement stm  = connection.prepareStatement(sql);
+            stm.setTimestamp(1,DateTimeHelper.getTimeStamp(from));
+            stm.setTimestamp(2,DateTimeHelper.getTimeStamp(to));
+            ResultSet rs = stm.executeQuery();
+            Employee currentEmp = new Employee();
+            currentEmp.setId(-1);
+            while(rs.next())
+            {
+                int eid = rs.getInt("eid");
+                if(eid != currentEmp.getId())
+                {
+                    currentEmp = new Employee();
+                    currentEmp.setId(eid);
+                    currentEmp.setName(rs.getString("ename"));
+                    emps.add(currentEmp);
+                    }
+                }
+                
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(EmployeeDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return emps;
+    }
 }
--- src/java/dal/HolidayDBContext.java
+++ src/java/dal/HolidayDBContext.java
@@ -4,10 +4,40 @@
  */
 package dal;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Holiday;
+
 /**
  *
  * @author hp
  */
-public class HolidayDBContext {
-    
+public class HolidayDBContext extends DBContext{
+    public ArrayList<Holiday> getHolidays (){
+        ArrayList<Holiday> holidays = new ArrayList<>();
+        try {
+            
+            
+            String sql = " select * from Holiday";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            ResultSet rs = stm.executeQuery();
+            while(rs.next()){
+                Holiday h = new Holiday();
+                h.setId(rs.getInt("hid"));
+                h.setName(rs.getString("hname"));
+                h.setFrom(rs.getDate("from"));
+                h.setTo(rs.getDate("to"));
+                h.setSalaryOT(rs.getFloat("salaryOT"));
+                holidays.add(h);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(HolidayDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return holidays;
+    }
 }
